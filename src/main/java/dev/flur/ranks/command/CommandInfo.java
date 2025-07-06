e53d937d52473b752e6c55904aae0ad62413839f
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2025-07-06 16:02:50 +0200

    V1.0.0 - initial commit

--- src/main/java/dev/flur/ranks/command/CommandInfo.java
+++ src/main/java/dev/flur/ranks/command/CommandInfo.java
@@ -1,4 +1,14 @@
 package dev.flur.ranks.command;
 
-public class CommandInfo {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CommandInfo {
+    String name();
+    String description() default "";
+    String permission() default "";
 }
